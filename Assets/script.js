0234ad2c43800e56ac5826dad339967f70a33733
Shannon Quinn <shannon.quinn91@gmail.com>
2020-10-13 13:26:06 -0400

    Starter JS code

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+var quiz = [
+    {
+        question: "1. HTML stands for:",
+        optionA: "a. Hypertext Markup Language",
+        optionB: "b. Hypertext Marketing Language",
+        optionC: "c. Hypercoding Marker Language",
+        optionD: "d. Hyperspeed Markup Language"
+    },
+    {
+        question: "2. CSS stands for:",
+        optionA: "a. Cool Style Sheet",
+        optionB: "b. Cool Story Sis",
+        optionC: "c. Cascading Style Sheets",
+        optionD: "d. Cascading Sorter Sheet"
+    }
+]
