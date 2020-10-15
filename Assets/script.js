2630b4e8cadf58468e9a1b5ca7f066bbac13e945
Shannon Quinn <shannon.quinn91@gmail.com>
2020-10-15 12:45:13 -0400

    Changes to CSS and addded event listener

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,25 @@
 var quiz = [
     {
         question: "1. HTML stands for:",
-        optionA: "a. Hypertext Markup Language",
-        optionB: "b. Hypertext Marketing Language",
-        optionC: "c. Hypercoding Marker Language",
-        optionD: "d. Hyperspeed Markup Language"
+        options: ["a. Hypertext Markup Language", "b. Hypertext Marketing Language", "c. Hypercoding Marker Language", "d. Hyperspeed Markup Language"],
+        correctAnswer: 0
     },
     {
-        question: "2. CSS stands for:",
-        optionA: "a. Cool Style Sheet",
-        optionB: "b. Cool Story Sis",
-        optionC: "c. Cascading Style Sheets",
-        optionD: "d. Cascading Sorter Sheet"
+        question: "2. CSS stands for:", 
+        options: ["a. Cool Sytle Sheet", "b. Cool Story Sis", "c. Cascading Style Sheets", "d. Cascading Sorter Sheet"],
+        correctAnswer: 2
     }
 ]
+
+var quizIndex = 0;
+var startBtn = document.querySelector(".start-button"); 
+
+function renderQuestion (currentIndex){
+    var question = quiz[currentIndex].question;
+    return question;
+}
+
+startBtn.addEventListener('click', function(){
+    renderQuestion(quizIndex);
+    console.log('clicked');
+})
